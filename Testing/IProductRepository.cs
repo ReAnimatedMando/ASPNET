0982cd053025d057c0d816d4e57d82892d8e2c8b
Aaron Tanguma <the_guma@yahoo.com>
2026-01-12 17:08:29 -0700

    followed steps to set up ASPNET

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Products> GetAllProducts();
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Products> GetAllProducts()
+        {
+            return _conn.Query<Products>("SELECT * FROM Products;");
+        }
+    }
+}
